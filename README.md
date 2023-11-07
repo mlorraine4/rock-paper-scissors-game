93fac88ba1a45ed94b560df9bbb82461bd693c35
maria <maria.lorraine.silvia4@gmail.com>
2023-11-07 11:18:19 -0500

    updated read me, centered buttons

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# rock-paper-scissors-game created for The Odin Project assignment
+<h1 align='center'>Rock, Paper, Scissors</h1>
+<h2>Challenge a computer to a game of rock, paper, scissors! Be the first to get 5 points to win.</h2>
+
+## Rock, Paper, Scissors Game submission for [The Odin Project](https://www.theodinproject.com/lessons/foundations-rock-paper-scissors)
+
+# 👇 Live Demo 👇
+
+[Rock, Paper, Scissors](https://mlorraine4.github.io/rock-paper-scissors-game/)
+
+## Built with
+
+- HTML
+- CSS
+- JS
+
+## Rules
+- Rock beats scissors
+- Scissors beats paper
+- Paper beats rock
--- index.js
+++ index.js
@@ -1,60 +1,76 @@
-let playerScore = 0
-let computerScore = 0
-let button = document.querySelectorAll('#btn');
-var playerChoice = '';
-var computerChoice = '';
+let playerScore = 0;
+let computerScore = 0;
+let buttons = document.querySelectorAll(".btn");
+var playerChoice = "";
+var computerChoice = "";
 
-let results = document.getElementById('results')
-results.textContent = 'Player score: 0 Computer score: 0'
+let results = document.getElementById("results");
+results.textContent = "Player score: 0 Computer score: 0";
 
-for (i=0; i<button.length; i++) {
-  button[i].addEventListener('click', function() {
+for (i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
     playerChoice = this.innerHTML;
-    console.log(playerChoice);
     getRandomChoice();
     playRound();
-    results.textContent = 'Player score: ' + playerScore + ' Computer score: ' +computerScore
+    results.textContent =
+      "Player score: " + playerScore + " Computer score: " + computerScore;
     if (computerScore === 5) {
-      results.textContent = 'Computer wins the game!'
+      results.textContent = "Computer wins the game!";
+      restart();
     }
     if (playerScore === 5) {
-      results.textContent = 'Congrats! You win the game!'
+      results.textContent = "Congrats! You win the game!";
+      restart();
     }
   });
 }
 
+function restart() {
+  playerScore = 0;
+  playerChoice = "";
+  computerScore = 0;
+  computerChoice = "";
+}
+
 function getRandomChoice() {
-  let randomNumber = Math.floor(Math.random()* 3);
-    if (randomNumber === 0) {
-    computerChoice = 'rock';
-    } else if (randomNumber === 1) {
-      computerChoice = 'paper';
-    } else computerChoice = 'scissors';
-    console.log(computerChoice);
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    computerChoice = "rock";
+  } else if (randomNumber === 1) {
+    computerChoice = "paper";
+  } else computerChoice = "scissors";
 }
 
 function youWin() {
   ++playerScore;
-    alert('you win!');
+  alert("you win!");
+}
+
+function compWin() {
+  ++computerScore;
+  alert("you lose!");
 }
 
 function playRound() {
   if (computerChoice === playerChoice) {
-    alert('tie');
-  } else if (computerChoice === 'rock' && playerChoice === 'scissors') {
+    alert("tie");
+  }
+  if (computerChoice === "rock" && playerChoice === "scissors") {
     youWin();
-  } else if (computerChoice === 'rock' && playerChoice === 'paper') {
-    ++computerScore
-    alert('you lose!');
-  } else if (computerChoice === 'paper' && playerChoice === 'scissors') {
+  }
+  if (computerChoice === "rock" && playerChoice === "paper") {
+    compWin();
+  }
+  if (computerChoice === "paper" && playerChoice === "scissors") {
     youWin();
-  } else if (computerChoice === 'paper' && playerChoice === 'rock') {
-    ++computerScore
-    alert('you lose!');
-  } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
+  }
+  if (computerChoice === "paper" && playerChoice === "rock") {
+    compWin();
+  }
+  if (computerChoice === "scissors" && playerChoice === "rock") {
     youWin();
-  } else if (computerChoice === 'scissors' && playerChoice === 'paper') {
-    ++computerScore
-    alert('you lose!');
-  } else console.log('what?');
+  }
+  if (computerChoice === "scissors" && playerChoice === "paper") {
+    compWin();
+  }
 }
